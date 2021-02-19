b0557d8449ee3c77678a6a877aa1a1e4f434f1b9
成雨 <rensailong@zhongan.io>
2021-02-19 16:52:19 +0800

    filemanager

--- lib/core/file.go
+++ lib/core/file.go
@@ -1,5 +1,51 @@
 package core
 
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	"gopkg.in/gographics/imagick.v3/imagick"
+)
+
+func init() {
+	imagick.Initialize()
+}
+
 type File struct {
-	Data string `json:"data"`
+	Data      string `json:"data"` // base64字符串
+	Name      string `json:"name"`
+	Size      string `json:"size"`
+	Extension string `json:"extension"` // 文件扩展名
+	mw        *imagick.MagickWand
+}
+
+func NewFile(fileJson string) (*File, error) {
+	file := &File{mw: imagick.NewMagickWand()}
+	if err := json.Unmarshal([]byte(fileJson), &file); err != nil {
+		return file, err
+	}
+	return file, nil
+}
+
+// 解析base64字符串
+func (f *File) decode() error {
+	if bytes, err := base64.StdEncoding.DecodeString(f.Data); err != nil {
+		// 读取文件内容
+		return f.mw.ReadImageBlob(bytes)
+	}
+	return nil
+}
+
+// 文件处理完毕之后将其写入至本地文件
+func (f *File) Write() error {
+	if err := f.decode(); err != nil {
+		return err
+	}
+	if err := f.mw.ResizeImage(uint(200), uint(200), imagick.FILTER_LANCZOS); err != nil {
+		return err
+	}
+	if err := f.mw.WriteImage("resize-file.png"); err != nil {
+		return err
+	}
+	return nil
 }
--- lib/core/filemanager.go
+++ lib/core/filemanager.go
@@ -1,25 +1,26 @@
 package core
 
-import (
-	"encoding/base64"
-	"encoding/json"
-)
-
 type Manager struct {
+	files []*File
 }
 
 func NewManager() *Manager {
 	return &Manager{}
 }
 
+// 将文件添加至"Manager"
 func (m *Manager) HandleFile(fileJson string) error {
-	file := &File{}
-	if err := json.Unmarshal([]byte(fileJson), &file); err != nil {
-		return err
-	}
-	_, err := base64.StdEncoding.DecodeString(file.Data)
-	if err != nil {
+	if file, err := NewFile(fileJson); err != nil {
+		m.files = append(m.files, file)
 		return err
 	}
 	return nil
+}
+
+// 并发处理文件
+func (m *Manager) Convert() (errs []error) {
+	if err := m.files[0].Write(); err != nil {
+		return append(errs, err)
+	}
+	return errs
 }
