0aa19c799b27c0972b974cbb57ded2499a2d13bc
成雨 <rensailong@zhongan.io>
2021-02-22 16:18:19 +0800

    update

--- frontend/src/views/Setting.vue
+++ frontend/src/views/Setting.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>setting</div>
+  <header class="m-2 flex justify-end items-center">
+    <router-link to="/" class="flex item-center text-blue-500 text-xl">
+      <RollbackOutlined />
+    </router-link>
+  </header>
+  <main class="m-2"></main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { RollbackOutlined } from "@ant-design/icons-vue";
 export default defineComponent({
   name: "Setting",
+  components: { RollbackOutlined },
   setup() {
     return {};
   }
